907ed081e54511bf431685a678d01de313cded3d
changkiyun <53373279+changkiyun@users.noreply.github.com>
2024-02-11 13:52:08 +0900

    [Bronze V] Title: A+B - 8, Time: 280 ms, Memory: 19060 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+
+        int T = s.nextInt();
+
+        for(int i=1; i<=T; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
+        }
+
+    }
+}
